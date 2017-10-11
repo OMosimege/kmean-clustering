c97cc7173aa27e7c3b9b302c12f5904fd5184dab
OMosimege <onalerona.mosimege@gmail.com>
2017-10-12 01:11:26 +0200

    added a create points method

--- kmean.h
+++ kmean.h
@@ -1,22 +1,29 @@
-#ifndef KMETHOD_H
-#define KMETHOD_H
+#ifndef KMEAN_H
+#define KMEAN_H
 #include <string>
 #include <iostream>
 #include <vector>
-#include <memory>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
+#include <cmath>
 
 namespace MSMONA001 {
 	//the >> and << operators to read/write the header info
-class kmethod {
+class kmean{
 
 	private:
 
 	public:
+		std::vector<std::pair<int, int>> points; //x number of points, coordinates x and y
+		std::vector<int> clusters; // k number of clusters
 
-		cretae_clusters();
-		calcMean();
-
-
+		kmean();//constructor
+		void create_points(); //make pairs for x point with x and y coordinates
+		int calcMean(); //method to calculate mean, return mean value
+		int calcDistance(); //method to claculate distance
+		void calcClustering(); //where main algorithm is
 
 }; //end of class
 
