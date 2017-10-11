5938734e4972c5f840692ac90f8c745ed8f10703
OMosimege <onalerona.mosimege@gmail.com>
2017-10-12 01:10:22 +0200

    added a create points method

--- kmean.cpp
+++ kmean.cpp
@@ -1,122 +1,59 @@
-#include "kmethod.h"
+#include "kmean.h"
 
 #include <string>
 #include <vector>
 #include <cmath>
-#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <fstream>
-#include <memory>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
 
 namespace MSMONA001{
-	 using namespace std;
+	using namespace std;
 
-   void create_clusters(){
+    kmean::kmean(){}
 
-    //saving old means
-    om1=m1;
-    om2=m2;
- 
-    //creating clusters
-    i1=i2=i3=0;
-    for(i1=0;i1<10;i1++)
-    {
-        //calculating distance to means
-        t1=k0[i1]-m1;
-        if(t1<0){t1=-t1;}
- 
-        t2=k0[i1]-m2;
-        if(t2<0){t2=-t2;}
- 
-        if(t1<t2)
-        {
-            //near to first mean
-            k1[i2]=k0[i1];
-            i2++;
+    void kmean::create_points(){//make pairs for x point with x and y coordinates
+        string line;
+        int i=0; //use as counter to add to vector
+        ifstream myfile ("input.txt");
+        if (myfile.is_open()){
+            while (getline(myfile,line)){
+                std::istringstream buffer(line);
+                int x;
+                int y;
+                buffer>>x>>ws>>y;
+
+                kmean::points[i]= make_pair(x,y);
+                i++;
+            }
+            myfile.close();
         }
-        else
-        {
-            //near to second mean
-            k2[i3]=k0[i1];
-            i3++;
+
+        else cout << "Unable to open file";
+
+        for(int j=0; j<kmean::points.size(); j++){
+            cout << kmean::points[j];
+
         }
- 
-    }
-   } //end of function
 
-   void calcMean(){
-    for(t1=0;t1<i2;t1++)
-    {
-        t2=t2+k1[t1];
-    }
-    m1=t2/i2;
- 
-    t2=0;
-    for(t1=0;t1<i3;t1++)
-    {
-        t2=t2+k2[t1];
-    }
-    m2=t2/i3;
- 
-    //printing clusters
-    cout<<"\nCluster 1:";
-    for(t1=0;t1<i2;t1++)
-    {
-        cout<<k1[t1]<<" ";
-    }
-    cout<<"\nm1="<<m1;
- 
-    cout<<"\nCluster 2:";
-    for(t1=0;t1<i3;t1++)
-    {
-        cout<<k2[t1]<<" ";
-    }
-    cout<<"\nm2="<<m2;
+    } 
 
-  
-   }//end of function
+    int kmean::calcMean(){ //method to calculate mean, return mean value
 
+    } 
+        
+    int kmean::calcDistance(){ //method to claculate distance
 
+    } 
+        
+    void kmean::calcClustering(){ //where main algorithm is
 
-
- 
-int main(){
-  int i1,i2,i3,t1,t2;
- 
-    int k0[10];
-    int k1[10];
-    int k2[10];
- 
-    cout<<"\nEnter 10 numbers:\n";
-    for(i1=0;i1<10;i1++)
-    {
-        cin>>k0[i1];
     }
-    //initial means
-    int m1;
-    int m2;
- 
-    cout<<"\n Enter initial mean 1:";
-    cin>>m1;
-    cout<<"\n Enter initial mean 2:";
-    cin>>m2;
-     
-    int om1,om2;    //old means
- 
-    
- 
-    t2=0;
-    //calculating new mean
-    
- 
-    cout<<"\n ----";
-    while(m1!=om1&&m2!=om2);
- 
-    cout<<"\n Clusters created";
- 
-    //ending
-    getch();
-}
+
+   
 
 }//end of namespace
