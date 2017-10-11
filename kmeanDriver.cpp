eef3935f3e9b26756917fa27e8e6b0e9b7ed9884
OMosimege <onalerona.mosimege@gmail.com>
2017-10-12 01:13:11 +0200

    created a driver file

---
+++ kmeanDriver.cpp
@@ -0,0 +1,19 @@
+#include "kmean.h"
+
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <memory>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    
+    MSMONA001::kmean kClustering; //create new object
+    kClustering.create_points();
+
+}
