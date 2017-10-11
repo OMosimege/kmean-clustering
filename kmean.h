5263fc19685980061406ec51ed8ef0e4bf893a47
OMosimege <onalerona.mosimege@gmail.com>
2017-10-12 00:26:14 +0200

    header file for kmean implementation

---
+++ kmean.h
@@ -0,0 +1,25 @@
+#ifndef KMETHOD_H
+#define KMETHOD_H
+#include <string>
+#include <iostream>
+#include <vector>
+#include <memory>
+
+namespace MSMONA001 {
+	//the >> and << operators to read/write the header info
+class kmethod {
+
+	private:
+
+	public:
+
+		cretae_clusters();
+		calcMean();
+
+
+
+}; //end of class
+
+}//end of namespace
+#endif
+
